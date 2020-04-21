779075aeb6f9b2a14c604311531f7ee16090b8fe
RobinLoksli <ami0504@mail.ru>
2020-04-21 23:56:35 +1000

    near full: problem with encode

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const xlsx = require('node-xlsx').default;
+const
+    xlsx = require('node-xlsx').default,
+    {PDFDocument, StandartFonts, rgb} = require('pdf-lib'),
+    fs   = require('fs')
 
 function Stud(param){
     this.lastName  = param.lastName;
@@ -8,19 +11,40 @@ function Stud(param){
     this.place     = param.place;
 }
 
-let
-    students = [],
-    data     = xlsx.parse(`${__dirname}/data.xlsx`)[0].data;
-
-for(var i = 1; i < data.length; i++){
-    let stud = new Stud({
-        lastName : data[i][0],
-        firstName: data[i][1],
-        patronyc : data[i][2],
-        group    : data[i][3],
-        place    : data[i][4],
-    });
-    students.push(stud);
+Stud.prototype.getFullData = function(){
+    return this.lastName + ' ' +
+           this.firstName + ' ' +
+           this.patronyc + ' ' +
+           this.group + ' ' +
+           this.place;
 }
 
-console.log(students);
+async function main(){
+
+    let
+        students = [],
+        pdfDoc   = await PDFDocument.create(),
+        data     = xlsx.parse(`${__dirname}/data.xlsx`)[0].data;
+
+    for(let i = 1; i < data.length; i++){
+        let stud = new Stud({
+            lastName : data[i][0],
+            firstName: data[i][1],
+            patronyc : data[i][2],
+            group    : data[i][3],
+            place    : data[i][4],
+        });
+        students.push(stud);
+    }
+
+    for(let i = 0; i < students.length; i++){
+        let page = pdfDoc.addPage();
+        page.drawText(students[i].getFullData(), {encode: 'utf-8'});
+    }
+
+    let save = await pdfDoc.save();
+    fs.writeFileSync('test.pdf', save);
+
+}
+
+main();
